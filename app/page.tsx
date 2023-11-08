dabab8d9b92c4f7be29db2b73308c0abd42b5b20
Ankit Sharma <ankitsharma290101@gmail.com>
2023-11-08 20:22:13 +0530

    themeProvider div

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-w-full justify-evenly">
      Blog App 1
-     <ThemeToggle/>
+     <div className="w-7 bg-purple-200 h-fit rounded-lg flex items-center justify-center" ><ThemeToggle/></div>
     </main>
   )
 }
