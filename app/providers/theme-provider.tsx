197f793ed9f55d306ea219cf0758497d9b1e4567
Ankit Sharma <ankitsharma290101@gmail.com>
2023-11-09 20:12:40 +0530

    themeProvider

--- app/providers/theme-provider.tsx
+++ app/providers/theme-provider.tsx
@@ -1,15 +1,13 @@
-'use client'
+"use client";
 
 import { ThemeProvider } from "next-themes";
 
-export function LightDarkProvider({
-    children}:{children:React.ReactNode}){
-    
-    return ( <ThemeProvider
-    enableSystem={true} attribute="class">
-        {children}
-        
-    </ThemeProvider> );
+export function NextThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider enableSystem={false} attribute="class" defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
 }
- 
-export default LightDarkProvider;
+
+export default NextThemeProvider;
