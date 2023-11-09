dad8c720bde70fa95ed41b8ac570027b43cf30e7
Ankit Sharma <ankitsharma290101@gmail.com>
2023-11-09 20:08:14 +0530

    next-auth-provider

--- app/context/index.tsx
+++ app/context/index.tsx
@@ -1,23 +1,30 @@
-'use client'
+"use client";
 
-import { Dispatch, ReactNode, SetStateAction, createContext, useState } from "react"
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from "react";
 
-type ContextType={
-    loading:boolean;
-    setLoading:Dispatch<SetStateAction<boolean>>
-}
-const initialstate={
-    loading:false,
-    setLoading:()=>{}
+type ContextType = {
+  loading: boolean;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+};
+const initialstate = {
+  loading: false,
+  setLoading: () => {},
+};
 
-}
+export const GlobalContext = createContext<ContextType>(initialstate);
 
-export const GlobalContext = createContext<ContextType>(initialstate)
+export default function GlobalState({ children }: { children: ReactNode }) {
+  const [loading, setLoading] = useState(false);
 
-export default function GlobalState({children}:{children:ReactNode}){
-    const [loading,setLoading] = useState(false)
-
-    return (
-        <GlobalContext.Provider value={{loading,setLoading}}>{children}</GlobalContext.Provider>
-    )
+  return (
+    <GlobalContext.Provider value={{ loading, setLoading }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
